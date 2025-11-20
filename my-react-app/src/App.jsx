b63febf72dd7d72f4faaf18af0e28fcf61e10332
Manan Jain <jainmanan2409@gmail.com>
2025-11-20 19:15:16 +0530

    Learning Card Components without CSS in same app

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './Header'
 import Footer from './Footer';
 import Sandwich from './Sandwich';
+import Cards from './Cards';
 
 function App() {
   return(
     <>
 
+    <Cards/>
     <Header/>
     <Sandwich/>
     <Footer/>
---
+++ my-react-app/src/Cards.jsx
@@ -0,0 +1,13 @@
+
+
+function Cards() {
+    return(
+        <div>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4z4eKK8vb9aAgHOh2wCfyyZ9Vr9WFrvBsvQ&s" alt="profile picture" />
+            <h2>Card Component Bot</h2>
+            <p>Hi, this is practice for making card components in React</p>
+        </div>
+    );
+}
+
+export default Cards
